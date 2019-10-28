951c048cb7d2d78c207f6132a8fb96923fb0737c
Micah Zaayer and Sofia Giordano <sofia@>
2019-10-28 14:26:14 -0700

    add second round of seed data.

---
+++ Shrooms/Migrations/20191028212431_MushData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shrooms.Migrations
+{
+    public partial class MushData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Mushrooms",
+                columns: new[] { "MushroomId", "CapDimensions", "Family", "Name", "Poisonous", "Psycho", "Region", "SporePrintDetails", "StemDimensions", "VisualDetails" },
+                values: new object[,]
+                {
+                    { 2, 6, "Stinkhorn", "Clathrus Ruber", true, false, "Southern Europe, Macronesia, Western Turkey, North Africa, Western Asia", "N/A", 14, "Round or oval hollow sphere, with latticed branches. Red or orange" },
+                    { 3, 3, "Hymenogastraceae", "Psilocybe cyanescens", false, true, "Pacific Northwest, Western Europe, Central Europe, New Zealand and west Asia", "blackish-brown to purple", 7, "Cap is caramel to chestnut-brown when moist, fading to pale buff or slightly yellowish when dried. Stems are wavy. Parts of mushroom can turn blue when touched or disturbed" },
+                    { 4, 3, "Hymenogastraceae", "Psilocybe cyanescens", false, true, "Pacific Northwest, Western Europe, Central Europe, New Zealand and west Asia", "blackish-brown to purple", 7, "Cap is caramel to chestnut-brown when moist, fading to pale buff or slightly yellowish when dried. Stems are wavy. Parts of mushroom can turn blue when touched or disturbed" },
+                    { 5, 29, "Polyporaceae", "Polypore", false, false, "Globally in forested areas", "N/A", 29, "woody, shelf-like form, projecting straight out of tree trunks or logs." }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Mushrooms",
+                keyColumn: "MushroomId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Mushrooms",
+                keyColumn: "MushroomId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Mushrooms",
+                keyColumn: "MushroomId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Mushrooms",
+                keyColumn: "MushroomId",
+                keyValue: 5);
+        }
+    }
+}
--- Shrooms/Models/ShroomsContext.cs
+++ Shrooms/Models/ShroomsContext.cs
@@ -15,8 +15,16 @@ namespace Shrooms.Models
         {
         builder.Entity<Mushroom>()
             .HasData(
-                new Mushroom { MushroomId = 1, Name = "Fly Amanita", Family = "Pluteaceae", Psycho = true, Poisonous = true, Region = "North America, Europe, Asia", SporePrintDetails = "na", VisualDetails = "white stem, red cap with white spots", CapDimensions = 9, StemDimensions = 13 }
+                new Mushroom { MushroomId = 1, Name = "Fly Amanita", Family = "Pluteaceae", Psycho = true, Poisonous = true, Region = "North America, Europe, Asia", SporePrintDetails = "na", VisualDetails = "white stem, red cap with white spots", CapDimensions = 9, StemDimensions = 13 },
+                
+                new Mushroom { MushroomId = 2, Name = "Clathrus Ruber", Family = "Stinkhorn", Psycho = false, Poisonous = true, Region = "Southern Europe, Macronesia, Western Turkey, North Africa, Western Asia", SporePrintDetails = "N/A", VisualDetails = "Round or oval hollow sphere, with latticed branches. Red or orange", CapDimensions = 6, StemDimensions = 14 },
 
+                new Mushroom { MushroomId = 3, Name = "Psilocybe cyanescens", Family = "Hymenogastraceae", Psycho = true, Poisonous = false, Region = "Pacific Northwest, Western Europe, Central Europe, New Zealand and west Asia", SporePrintDetails = "blackish-brown to purple", VisualDetails = "Cap is caramel to chestnut-brown when moist, fading to pale buff or slightly yellowish when dried. Stems are wavy. Parts of mushroom can turn blue when touched or disturbed", CapDimensions = 3, StemDimensions = 7 },
+
+                new Mushroom { MushroomId = 4, Name = "Psilocybe cyanescens", Family = "Hymenogastraceae", Psycho = true, Poisonous = false, Region = "Pacific Northwest, Western Europe, Central Europe, New Zealand and west Asia", SporePrintDetails = "blackish-brown to purple", VisualDetails = "Cap is caramel to chestnut-brown when moist, fading to pale buff or slightly yellowish when dried. Stems are wavy. Parts of mushroom can turn blue when touched or disturbed", CapDimensions = 3, StemDimensions = 7 },
+                new Mushroom { MushroomId = 5, Name = "Polypore", Family = "Polyporaceae", Psycho = false, Poisonous = false, Region = "Globally in forested areas", SporePrintDetails = "N/A", VisualDetails = "woody, shelf-like form, projecting straight out of tree trunks or logs.", CapDimensions = 29, StemDimensions = 29 }
+
+                
             );
         }
     }
