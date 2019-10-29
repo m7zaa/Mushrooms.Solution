32445d6c1351cb6a353472d0c4e4768408989a6f
Student <example@example.com>
2019-10-29 13:18:15 -0700

    add token authentication.

--- Shrooms/Models/Mushroom.cs
+++ Shrooms/Models/Mushroom.cs
@@ -4,23 +4,23 @@ namespace Shrooms.Models
     public class Mushroom
     {
         public int MushroomId { get; set; }
-        [Required]
+        
         public string Name { get; set; }
-        [Required]
+        
         public string Family { get; set; }
-        [Required]
+        
         public bool Psycho { get; set; }
-        [Required]
+        
         public bool Poisonous { get; set; }
-        [Required]
+        
         public string Region { get; set; }
-        [Required]
+        
         public string SporePrintDetails { get; set; }
-        [Required]
+        
         public string VisualDetails { get; set; }
-        [Required]
+        
         public int CapDimensions { get; set; }
-        [Required]
+        
         public int StemDimensions { get; set; }
 
     
