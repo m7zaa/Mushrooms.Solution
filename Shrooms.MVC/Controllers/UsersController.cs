1d4c703b3f68492954f8f7a8ba7555f8a9ccca5a
Student <example@example.com>
2019-10-31 15:19:41 -0700

    add white footer.

---
+++ Shrooms.MVC/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Threading.Tasks;
+  using System;
+  using Microsoft.AspNetCore.Authorization;
+  using Microsoft.AspNetCore.Mvc.Rendering;
+  using Microsoft.AspNetCore.Mvc;
+  using Microsoft.EntityFrameworkCore;
+  using Shrooms.Models;
+
+  namespace Shrooms.Controllers {
+      public class UsersController : Controller {
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      }
+  }
